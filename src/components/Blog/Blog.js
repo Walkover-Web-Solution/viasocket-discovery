a263b171abebdabeec130e87dfb9284c9933f709
Rohit Mirchandani <rohitmirchandani365@gmail.com>
2024-09-20 11:28:54 +0530

    opening new blog in new tab

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,28 +1,23 @@
-import { useRouter } from 'next/router';
 import styles from '@/components/Blog/Blog.module.css';
 
-
 export default function BlogCard({ blog }) {
-  const router = useRouter();
-
-  const handleClick = () => {
-    router.push(`/blog/${blog._id}`);
-  };
-
   return (
-    <div key={blog._id} className={styles.card} onClick={handleClick}>
-      <h3>{blog.title}</h3>
-      <p>{blog.blog.find(section => section.section === 'introduction')?.content}</p>
-      <div className={styles.tagsContainer}>
-        {blog.tags.map((tag, index) => (
-          <button
-            key={index}
-            className={`${styles.tag} ${styles[tag.toLowerCase()]}`}
-          >
-            {tag}
-          </button>
-        ))}
-      </div>
+    <div key={blog._id} className={styles.card}>
+      <a href={`/discovery/blog/${blog._id}`} target="_blank" rel="noopener noreferrer">
+        <h3>{blog.title}</h3>
+        <p>{blog.blog.find(section => section.section === 'introduction')?.content}</p>
+        <div className={styles.tagsContainer}>
+          {blog.tags.map((tag, index) => (
+            <button
+              key={index}
+              className={`${styles.tag} ${styles[tag.toLowerCase()]}`}
+            >
+              {tag}
+            </button>
+          ))}
+        </div>
+      </a>
     </div>
   );
 }
+
