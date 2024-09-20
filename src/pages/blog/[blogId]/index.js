108cd5050f257af31e29d6e757857d45aa25984c
Rohit Mirchandani <rohitmirchandani365@gmail.com>
2024-09-20 14:30:41 +0530

    increasing cache time on blog page

--- src/pages/blog/[blogId]/index.js
+++ src/pages/blog/[blogId]/index.js
@@ -4,44 +4,45 @@ import blogServices from '@/services/blogServices';
 import { fetchIntegrations, getUserById } from '@/utils/apiHelper';
 import styles from './blogPage.module.scss';
 import { useEffect, useState } from 'react';
+
 export async function getServerSideProps(context) {
   const { blogId } = context.params;
   const props = {};
+  
+  // Set Cache-Control headers to cache for 24 hours (86400 seconds)
+  context.res.setHeader('Cache-Control', 'public, s-maxage=86400, stale-while-revalidate=59');
+
   try {
     const blog = await blogServices.getBlogById(blogId);
     const user = await getUserById(blog?.createdBy);
     props.blog = blog;
-    props.user=user;
-    // try{
-    //   const integrations = await getIntegrations(blog.apps);
-    //   props.integrations = integrations;
-    // }catch(error){
-    //   console.error('Error fetching integrations:', error);
-    // }
+    props.user = user;
   } catch (error) {
-    console.error('Error fetching blog data:', error); // Return an empty object if there's an error
+    console.error('Error fetching blog data:', error);
   }
+
   return { props };
 }
 
-export default function BlogPage({ blog, user}) {
+export default function BlogPage({ blog, user }) {
   const [integrations, setIntegrations] = useState(null);
+
   useEffect(() => {
     const getData = async (apps) => {
-        const data = await fetchIntegrations(apps)
-        setIntegrations(data);
-      
-    }
+      const data = await fetchIntegrations(apps);
+      setIntegrations(data);
+    };
     if (blog?.apps) {
-      getData(blog?.apps)
+      getData(blog?.apps);
     }
   }, [blog?.apps]);
+
   return (
     <div>
       <div className={styles.container}>
-        <AIresponse blogData={blog} user={user} integrations = {integrations}/>
+        <AIresponse blogData={blog} user={user} integrations={integrations} />
       </div>
-      <Integrations integrations = {integrations} />
+      <Integrations integrations={integrations} />
     </div>
   );
 }
