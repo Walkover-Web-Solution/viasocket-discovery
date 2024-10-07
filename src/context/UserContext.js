28f00d8469d717156f2f4d3706402a3c8256fa75
tanish <tanish@whozzat.com>
2024-10-07 15:36:47 +0530

    user session managed from viasocket

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,4 +1,5 @@
-import { getUserDataFromLocalStorage } from '@/utils/storageHelper';
+import { getCurrentUser } from '@/utils/apiHelper';
+import { getCurrentEnvironment, getFromCookies, getUserDataFromLocalStorage } from '@/utils/storageHelper';
 import { createContext, useState, useContext, useEffect } from 'react';
 
 const UserContext = createContext();
@@ -7,12 +8,23 @@ export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const [isChatOpen,setIsChatOpen]=useState(false);
-  useEffect(() => {
+  const getCurrentUserFunction = async  () => {
     const storedUser = getUserDataFromLocalStorage();
     if (storedUser) {
       setUser(storedUser);
+      setLoading(false);
+      return ;
     }
+    const token = getFromCookies(getCurrentEnvironment())
+    if (!token) return 
+    const userInfo = await getCurrentUser();
+    const userData = userInfo?.data[0]
+    localStorage.setItem("userDetail", JSON.stringify({ name: userData.name, email: userData.email, id: userData.id }));
+    setUser({ name: userData.name, email: userData.email, id: userData.id })
     setLoading(false);
+  }
+  useEffect(() => {
+    getCurrentUserFunction()
   }, []);
   return (
     <UserContext.Provider value={{ user, setUser, loading ,isChatOpen ,setIsChatOpen}}>
