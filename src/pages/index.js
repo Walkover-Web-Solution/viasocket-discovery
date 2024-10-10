bc9953793e5c1a150b40032d8c0fec6c603fcc0a
Rohit Mirchandani <rohitmirchandani365@gmail.com>
2024-10-10 15:56:53 +0530

    handling heading on chatbot open

--- src/pages/index.js
+++ src/pages/index.js
@@ -121,9 +121,15 @@ export default function Home() {
       setIsOpen(true);
     }
     return (
-        <>            
-          <h1 className={styles.homeTitle}>Get the Best Business Tools – Handpicked by Real Businesses Like Yours</h1>
-          <p className={styles.homep}>{'Discover top tools curated by real businesses that have been where you are. Tailored to your industry, size, location, and goals – whether you\'re scaling up, boosting efficiency, or cutting costs, find the tools that fit your needs without the hassle.'}</p>
+        <>
+          {
+            !isOpen && (
+              <>
+                <h1 className={styles.homeTitle}>Get the Best Business Tools – Handpicked by Real Businesses Like Yours</h1>
+                <p className={styles.homep}>{'Discover top tools curated by real businesses that have been where you are. Tailored to your industry, size, location, and goals – whether you\'re scaling up, boosting efficiency, or cutting costs, find the tools that fit your needs without the hassle.'}</p>
+              </>
+            )
+          }            
           <div className={styles.postHeaderDiv}>
               {searchQuery && !isOpen ? (
                   renderBlogsSection(searchResults, searchQuery, true)
