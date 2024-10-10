d756ee96b2e9ffa6b875feda40b636efc55d3408
Rohit Mirchandani <rohitmirchandani365@gmail.com>
2024-10-10 19:10:02 +0530

    added icons on homepage

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
-import styles from '@/components/Blog/Blog.module.css';
+import styles from '@/components/Blog/Blog.module.scss';
+import { Avatar } from '@mui/material';
 
 export default function BlogCard({ blog, isLoading, className }) {
 
@@ -17,12 +18,23 @@ export default function BlogCard({ blog, isLoading, className }) {
       </div>
     );
   }
-
   return (
     <div key={blog.id} className={`${styles.card} ${className || ''}`}>
       <a href={blog.url || `/discovery/blog/${blog.id}`} target="_blank" rel="noopener noreferrer">
         <h3>{blog.title}</h3>
-        <p>{blog.introduction || blog.blog?.find(section => section.section === 'introduction')?.content}</p>
+        {/* <p>{blog.introduction || blog.blog?.find(section => section.section === 'introduction')?.content}</p> */}
+        <div className={styles.appIconsDiv}>
+          {Object.entries(blog?.apps||{}).map(([appName, {iconUrl}], index) => {
+            if(!iconUrl) return null;
+            return (<Avatar 
+              key={index}
+              className={styles.appIcon}
+              src={iconUrl}
+              alt={appName}
+              variant = 'square'
+            />
+          )})}
+        </div>
         <div className={styles.tagsContainer}>
           {blog.tags?.map((tag, index) => (
             <button
