2a3db20871270c67496d3d03cdbe26bb6118ddc8
Rohit Mirchandani <rohitmirchandani365@gmail.com>
2024-10-19 12:22:41 +0530

    viasocket header footer added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@
 import { useUser } from "@/context/UserContext";
 import { useEffect, useState } from "react";
 import { FaUserCircle } from "react-icons/fa";
+import UserDetail from "../UserDetailPopup/UserDetailPopup";
 
 export default function Header() {
   const { user } = useUser();
@@ -16,36 +17,48 @@ export default function Header() {
     setShowUserInfo(!showUserInfo);
   };
   return (
-    <div class="container d-flex align-items-center justify-content-between py-5">
-      <a href="https://viasocket.com">
-        <img src="https://viasocket.com/assets/brand/logo.svg" alt="Logo" />
-      </a>
-      <div class="d-flex align-items-center gap-4">
-        <a class="text-decoration-none text-dark" target="_blank" href="/">
-          Home
-        </a>
+    <div
+    class="container d-flex align-items-center justify-content-between py-5"
+    >
+    <img src="https://viasocket.com/assets/brand/logo.svg" alt="Logo" />
+    <div class="d-flex align-items-center gap-4">
+        <a class="text-decoration-none text-dark" target="_blank" href="/">Home</a>
         <a
-          class="text-decoration-none text-dark"
-          target="_blank"
-          href="https://viasocket.com/faq/pricing"
+        class="text-decoration-none text-dark"
+        target="_blank"
+        href="https://viasocket.com/faq/pricing"
+        >Pricing</a
         >
-          Pricing
-        </a>
         <a
-          class="text-decoration-none text-dark"
-          target="_blank"
-          href="https://careers.walkover.in/jobs/Careers"
+        class="text-decoration-none text-dark"
+        target="_blank"
+        href="https://careers.walkover.in/jobs/Careers"
+        >We are hiring</a
         >
-          We are hiring
-        </a>
         <a
-          class="text-decoration-none text-dark"
-          target="_blank"
-          href="https://viasocket.com/support"
+        class="text-decoration-none text-dark"
+        target="_blank"
+        href="https://viasocket.com/support"
+        >Support</a
         >
-          Support
-        </a>
-      </div>
+        <div class = "position-relative">
+            {isLoggedIn ? (
+                <>
+                    <strong class="text-dark" style = {{cursor: "pointer"}} onClick = {toggleUserInfo}> {user.email} </strong>
+                    <UserDetail isOpen={showUserInfo} onClose={toggleUserInfo} />
+                </>
+            ) : (
+                <div class="d-flex align-items-center gap-4">
+                    <a href="https://viasocket.com/login">
+                        <button class="btn btn-outline-dark">Login</button>
+                    </a>
+                    <a href="https://viasocket.com/signup">
+                        <button class="btn btn-dark text-white">Sign Up</button>
+                    </a>
+                </div>
+            )}
+        </div>
     </div>
-  );
+    </div>
+  )
 }
