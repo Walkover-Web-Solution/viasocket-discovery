42519854799bf9ca6f1a920e45d2000c23b8afc3
Gourav Choudhary <gouravcc1@gmail.com>
2024-11-14 14:41:43 +0530

    Refactor MongoDB schema to handle dot notation in keys

--- models/BlogModel.js
+++ models/BlogModel.js
@@ -1,5 +1,6 @@
 // models/Blog.js
 import mongoose from 'mongoose';
+import { replaceDotsInKeys, restoreDotsInKeys } from '@/utils/utils';
 const createBlogModel = (connection) => {
   const BlogSchema = new mongoose.Schema({
     id: {
@@ -27,6 +28,31 @@ const createBlogModel = (connection) => {
   },
   createdBy: Array,
   }, { minimize: false });
+
+  BlogSchema.pre('save', function (next) {
+    if (this.apps) {
+      this.apps = replaceDotsInKeys(this.apps);
+    }
+    next();
+  });
+  
+  BlogSchema.pre('findOneAndUpdate', function (next) {
+    const update = this.getUpdate();
+    if (update && update.apps) {
+      update.apps = replaceDotsInKeys(update.apps);
+    }
+    next();
+  });
+  
+  BlogSchema.post('find', function(docs) {
+    if (Array.isArray(docs)) {
+      docs.forEach(doc => {
+        if (doc.apps) {
+          doc.apps = restoreDotsInKeys(doc.apps);
+        }
+      });
+    }
+  });
 
   BlogSchema.index({ id: 1 });
 
