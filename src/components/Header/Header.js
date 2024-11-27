722633e0ebed262dec7b8b8fe702aa569a063aae
Gourav Choudhary <gouravcc1@gmail.com>
2024-11-27 12:01:19 +0530

    header responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,17 @@
-// import  useUser  from "@/context/UserContext";
 import { useUser } from "@/context/UserContext";
-import { useEffect, useState } from "react";
-import { FaUserCircle } from "react-icons/fa";
+import { useEffect, useState, useRef } from "react";
+import { FaBars } from "react-icons/fa";
 import UserDetail from "../UserDetailPopup/UserDetailPopup";
 import styles from "./Header.module.scss";
 import { setPathInLocalStorage } from "@/utils/storageHelper";
 
 export default function Header() {
   const { user } = useUser();
-
   const [showUserInfo, setShowUserInfo] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const menuRef = useRef(null); 
+
   useEffect(() => {
     setIsLoggedIn(user ? true : false);
   }, [user]);
@@ -21,15 +22,42 @@ export default function Header() {
   const toggleUserInfo = () => {
     setShowUserInfo(!showUserInfo);
   };
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setIsMobileMenuOpen(false); 
+      }
+    };
+
+    if (isMobileMenuOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isMobileMenuOpen]);
+
   return (
     <div className = {styles.headerDiv}>
     <div
     className ={`container d-flex align-items-center justify-content-between py-5 ${styles.headerDiv}`}
     >
     <a href="https://viasocket.com">
-      <img src="https://viasocket.com/assets/brand/logo.svg" alt="Logo" />
+      <img className={styles.viasocketLogo} src="https://viasocket.com/assets/brand/logo.svg" alt="Logo" />
     </a>
-    <div class="d-flex align-items-center gap-4">
+        <div className={styles.mobileMenuIcon} onClick={toggleMobileMenu}>
+          {!isMobileMenuOpen && <FaBars size={24} />}
+        </div>
+
+        <div ref={menuRef} className={`${styles.navLinks} ${isMobileMenuOpen ? styles.showMobileMenu : ""}`}>
         <a class="text-decoration-none text-dark" target="_blank" href="/">Home</a>
         <a
         class="text-decoration-none text-dark"
