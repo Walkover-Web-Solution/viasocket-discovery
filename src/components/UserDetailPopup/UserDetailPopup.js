829f6fef74ec97909aa6ca82735d9a65a4acd3f9
Gourav Choudhary <gouravcc1@gmail.com>
2024-12-24 10:43:01 +0530

    added user name link

--- src/components/UserDetailPopup/UserDetailPopup.js
+++ src/components/UserDetailPopup/UserDetailPopup.js
@@ -3,6 +3,7 @@ import { clearUserData } from '@/utils/storageHelper';
 import { useUser } from '@/context/UserContext';
 import { useRouter } from 'next/router';
 import { ClickAwayListener } from '@mui/material';
+import { nameToSlugName } from "@/utils/utils";
 
 const UserDetail = ({ isOpen, onClose }) => {
     const {user , setUser}=useUser();
@@ -31,7 +32,7 @@ const UserDetail = ({ isOpen, onClose }) => {
                 <div className={styles.userDetails}>
                         <>
                             <p><b>{user?.name || ""}</b></p>
-                            <p><b>{user?.email || ""}</b></p>
+                            <a href={`/discovery/user/${user?.id}/${nameToSlugName(user?.name)}`}><p><b>{user?.email || ""}</b></p></a>
                             <button className={styles.logoutButton} onClick={handleLogout}>Logout</button>
                         </>
                 </div>
