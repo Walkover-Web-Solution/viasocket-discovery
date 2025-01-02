9d7cd775ddcc16b01ebb8ccd12b32169dd1d2b35
Rohit Mirchandani <rohitmirchandani365@gmail.com>
2025-01-02 11:04:56 +0530

    fix auth popup after login

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,47 @@
+import { useUser } from "@/context/UserContext";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import styles from './Layout.module.scss'
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import UnauthorizedPopup from "../UnauthorisedPopup/UnauthorisedPopup";
+import UserBioPopup from "../UserBioPopup/UserBioPoup";
+import { dispatchAskAiEvent } from "@/utils/utils";
 
 export default function Layout({ children }) {
   const router = useRouter();
+  const {user} = useUser();
+  const [userBioPopup, setUserBioPopup] = useState(false);
+  const [unAuthPopup, setUnAuthPopup] = useState(false);
   const isHomePage = router.pathname === '/';
+  
+  useEffect(() => {
+    if (typeof document === 'undefined') return ;
+    const handleEvent = async (e) => {
+      if (!user) {
+        setUnAuthPopup(true);
+        return;
+      }
+      if(!user.meta?.bio){
+        setUserBioPopup(true);
+        return;
+      }
+      if(e.detail?.callback)
+        e.detail.callback();
+    }
+    window.addEventListener('askAppAiWithAuth', handleEvent);
+    return () => {
+      window.removeEventListener('askAppAiWithAuth', handleEvent);
+    }
+  }, [user]);
+
+
   return (
     <>
       <Header />
       <main className = {styles.main}>{children}</main>
+      <UnauthorizedPopup isOpen={unAuthPopup} onClose={()=>setUnAuthPopup(false)} />
+			<UserBioPopup isOpen={userBioPopup} onClose={()=>setUserBioPopup(false)}/>
       {!isHomePage &&  
         <Footer />
       }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,40 +1,17 @@
 // pages/_app.js
 // import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Layout from '@/components/Layout/Layout';
-import { UserProvider, useUser } from '@/context/UserContext';
-import React, { useEffect, useState } from 'react';
+import { UserProvider } from '@/context/UserContext';
+import React, { useEffect } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'; // Import the CSS
 import '../globals.scss';
 import Head from 'next/head';
-import UnauthorizedPopup from '@/components/UnauthorisedPopup/UnauthorisedPopup';
-import UserBioPopup from '@/components/UserBioPopup/UserBioPoup';
 
 function MyApp({ Component, pageProps }) {
-  const {user} = useUser();
-  const [userBioPopup, setUserBioPopup] = useState(false);
-  const [unAuthPopup, setUnAuthPopup] = useState(false);
   useEffect(() => {
     typeof document !== 'undefined' &&
       import('bootstrap/dist/js/bootstrap.bundle.min.js');
-      const handleEvent = async (e) => {
-        if (!user) {
-          setUnAuthPopup(true);
-          return;
-        }
-        if(!user.meta?.bio){
-          setUserBioPopup(true);
-          return;
-        }
-        setSearchQuery('');
-        dispatchAskAiEvent(e.detail.message);
-        if(e.detail?.callback)
-          e.detail.callback();
-      }
-      window.addEventListener('askAppAiWithAuth', handleEvent);
-      return () => {
-        window.removeEventListener('askAppAiWithAuth', handleEvent);
-      }
   }, []);
 
   return (
@@ -56,8 +33,6 @@ function MyApp({ Component, pageProps }) {
         embedToken={process.env.NEXT_PUBLIC_CHAT_BOT_TOKEN} 
         hideIcon="true"
       /> */}
-      <UnauthorizedPopup isOpen={unAuthPopup} onClose={()=>setUnAuthPopup(false)} />
-			<UserBioPopup isOpen={userBioPopup} onClose={()=>setUserBioPopup(false)}/>
     </>
   );
 }
