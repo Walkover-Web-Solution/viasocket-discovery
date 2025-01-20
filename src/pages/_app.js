2e5435476394b9c73b94935d4b1a9b88ebb308bd
Gourav Choudhary <gouravcc1@gmail.com>
2025-01-20 10:46:43 +0530

    script added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,17 +10,42 @@ import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    typeof document !== 'undefined' &&
+    if (typeof document !== 'undefined') {
       import('bootstrap/dist/js/bootstrap.bundle.min.js');
+    }
   }, []);
 
   return (
     <>
       <Head>
         <title>viaSocket Discovery</title>
-        <link rel="icon" type="image/png" sizes="32x32" href="/assets/brand/favicon-32x32.png"></link>
-        <link rel="icon" type="image/png" sizes="16x16" href="/assets/brand/favicon-16x16.png"></link>
-        <script type="text/javascript" async="" src="https://www.googletagmanager.com/gtag/js?id=G-0DFEMF0FTJ&amp;l=dataLayer&amp;cx=c&amp;gtm=45He51d0v9194133166za200"></script>
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/brand/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/brand/favicon-16x16.png"/>
+        <script type="text/javascript" async src="https://www.googletagmanager.com/gtag/js?id=G-0DFEMF0FTJ&l=dataLayer&cx=c&gtm=45He51d0v9194133166za200"></script>
+
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-THTCRSLN"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
+        <script>
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
+              var f = d.getElementsByTagName(s)[0],
+              j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+              j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+              f.parentNode.insertBefore(j, f);
+            })(window,document,'script','dataLayer','GTM-THTCRSLN');
+          `}
+        </script>
       </Head>
       <UserProvider>
         <Layout>
@@ -28,11 +53,14 @@ function MyApp({ Component, pageProps }) {
           <ToastContainer />
         </Layout>
       </UserProvider>
+
       {/* <Script
         id="chatbot-main-script"
         src="https://chatbot-embed.viasocket.com/chatbot-prod.js"
-        embedToken={process.env.NEXT_PUBLIC_CHAT_BOT_TOKEN} 
-        hideIcon="true"
+        strategy="lazyOnload"
+        onLoad={() => {
+          window.embedToken = process.env.NEXT_PUBLIC_CHAT_BOT_TOKEN;
+        }}
       /> */}
     </>
   );
